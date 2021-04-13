c07950df17ed924cccf407b0f896baddd2943f72
Shubham Anand <shubham_chess@live.com>
2021-04-14 05:05:47 +0530

    bug fix

--- server/api/discover.js
+++ server/api/discover.js
@@ -5,17 +5,20 @@ import { hasParams } from '../utils';
 
 async function discover(req, res) {
   const params = req.query;
-  await new Promise((resolve) => setTimeout(() => resolve(), 2000));
 
   if (!hasParams(params, ['username'])) {
     res.send({ success: false, error: 'Bad request' });
     return;
   }
 
-  const { username } = params;
+  const { username, query } = params;
 
   const user = await User.findOne({ username }) || {};
-  const apps = await App.find({});
+  const apps = await App.aggregate([{
+    $match: {
+      name: { $regex: query, $options: 'i' },
+    },
+  }]);
   const analyses = await Analysis.find({ appId: { $in: apps.map((app) => app.appId) } });
   const userAnalysis = await UserAnalysis.aggregate([
     {
